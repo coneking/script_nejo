8d2137a4db026750b620318f2c4b2b171f014764
Sergio Conejeros <sergio.conejeros@cencosud.cl>
2017-11-16 11:56:16 -0300

    Optimización del script

--- Bash/crea_solicitudes.sh
+++ Bash/crea_solicitudes.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-# Crea solicitudes o incidentes automáticamente
-cnt1=""
-loc1=""
-pcat1=""
-nr1=""
-resum1=""
-coment1=""
+# Listado con preguntas
+listado=(cnt
+loc
+pcat
+nr
+resumen
+comentario
+)
 
+# Contador por pregunta
+contador=0; 
+while [ $contador -le 5 ]; do
+	# Se recorre las preguntas
+	for i in ${listado[@]}; do
+		# Declara cada pregunta como variable
+		declare "$i="""; 
+		# Itera en las variables vacías
+		while [ -z ${!i} ]; do 
+			echo -e "Ingrese $i: \c"; 
+			read $i; 
+		done;
+	# Aumenta el contador en 1
+	let contador++; 
+	done; 
+done; 
 
-while [ -z cnt1 -o -z loc1 -o -z pcat1 -o -z nr1 -o -z coment1 ]; do
-	echo -e "Ingrese cnt (Usuario final afectado): \c"
-	read cnt1
-
-	echo -e "Ingrese loc (Ubicación): \c"
-	read loc1
-
-	echo -e "Ingrese pcat (Área de solicitud): \c"
-	read pcat1
-
-	echo -e "Ingrese nr (Servicio TI): \c"
-	read nr1
-	
-	echo -e "Ingrese resumen (Subjet): \c"
-	read resum1
-
-	echo -e "Ingrese un comentario: \c"
-	read coment1
-
-done
 coment2=$(echo $coment1 |sed 's/ /%20/g')
 
-
-echo "curl -X POST "¡¡¡INGRESAR URL!!!" -H 'cache-control: no-cache' -H 'content-type: application/x-www-form-urlencoded' -d 'attrs=status%3Bcrs%3A5200%3Bcustomer%3Bcnt%3A$cnt1%3Bassignee%3Bcnt%3ABE1C93DC22D14B4680C305256AD9840F%3Bpriority%3Bpri%3A505%3Btype%3Bcrt%3A182%3Bdescription%3B$coment2%3Bsummary%3B$resum1%3BzLocation%3Bloc%3A$loc1%3Baffected_resource%3Bnr%3A$nr1%3Bcategory%3Bpcat%3A$pcat1%3Bzreporting_met%3Brptmeth%3A7305%3Bgroup%3Bcnt%3A09D8517028B2434C8870CE33833D56F2&attrsRet=ref_num&creador=cnt%3ABE1C93DC22D14B4680C305256AD9840F'"
+echo "curl -X POST "¡¡¡INGRESAR URL!!!" -H 'cache-control: no-cache' -H 'content-type: application/x-www-form-urlencoded' -d 'attrs=status%3Bcrs%3A5200%3Bcustomer%3Bcnt%3A$cnt%3Bassignee%3Bcnt%3ABE1C93DC22D14B4680C305256AD9840F%3Bpriority%3Bpri%3A505%3Btype%3Bcrt%3A182%3Bdescription%3B$coment2%3Bsummary%3B$resum%3BzLocation%3Bloc%3A$loc%3Baffected_resource%3Bnr%3A$nr%3Bcategory%3Bpcat%3A$pcat%3Bzreporting_met%3Brptmeth%3A7305%3Bgroup%3Bcnt%3A09D8517028B2434C8870CE33833D56F2&attrsRet=ref_num&creador=cnt%3ABE1C93DC22D14B4680C305256AD9840F'"
